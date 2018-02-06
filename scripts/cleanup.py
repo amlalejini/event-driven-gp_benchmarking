adb5f1346a076be73ff0a15d9cb1da6ed7c1453a
Alex Lalejini <amlalejini@gmail.com>
2018-02-06 12:21:16 -0500

    Added a script to clean up HPCC data.

---
+++ scripts/cleanup.py
@@ -0,0 +1,40 @@
+'''
+cleanup.py
+This script cleans up unnecessary files from runs on HPCC.
+'''
+
+import argparse, os, copy, errno
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Experiment cleanup script.")
+    parser.add_argument("directory", type=str, help="Target directory to clean up.")
+    parser.add_argument("-u", "--update", type=int, help="Final update of experiment.")
+
+    final_update = args.update if args.update else default_final_update
+    exp_dir = args.directory
+    update = args.update
+
+    print "Experiment data directory: " + exp_dir
+    print "  Final update: " + str(final_update)
+
+    runs = [d for d in os.listdir(exp_dir) if "__" in d]
+    for run in runs:
+        print "Cleaning up run: " + str(run)
+        run_dir = os.path.join(exp_dir, run)
+        things = [d for d in os.listdir(run_dir)]
+        for thing in things:
+            if "pop_" in thing and thing != "pop_%d" % update:
+                cmd = "rm -rf %s" % os.path.join(run_dir, thing)
+                print "deleting: " + thing
+            if thing == "pattern_matching" or thing == "consensus" or thing == "changing_environment":
+                cmd = "rm %s" % thing
+                print "deleting: " + thing
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
--- scripts/exploration.R
+++ scripts/exploration.R
@@ -20,7 +20,7 @@ model.matrix(~treatment, data=ff_data)
 fit = lm(max_fitness ~ treatment, data=ff_data)
 
 kw_fit <- kruskal.test(max_fitness ~ treatment, data=ff_data)
-dt <- dunnTest(max_fitness~treatment, data=ff_data)
+dt <- dunnTest(max_fitness~treatment, data=ff_data, method="bonferroni")
 
 
 
@@ -61,4 +61,32 @@ dt_ce_4 <- dunnTest(fitness~treatment, data=ce_ff_data_4, method="bonferroni")
 
 kw_fit_ce_2 <- kruskal.test(fitness ~ treatment, data=ce_ff_data_2)
 dt_ce_2 <- dunnTest(fitness~treatment, data=ce_ff_data_2, method="bonferroni")
+
+######## Teasing apart the combined condition #########
+ce_tff_data <-  read.csv("/Users/amlalejini/DataPlayground/signal-gp-benchmarking/changing_environment/teaser_final_fitness.csv")
+
+ce_tff_data_2 <- ce_tff_data[grep("ENV2$", ce_tff_data$treatment),]
+ce_tff_data_2$treatment <- relevel(ce_tff_data_2$treatment, ref="ChgEnv_ED1_AS1_P125_ENV2")
+
+ce_tff_data_4 <- ce_tff_data[grep("ENV4$", ce_tff_data$treatment),]
+ce_tff_data_4$treatment <- relevel(ce_tff_data_4$treatment, ref="ChgEnv_ED1_AS1_P125_ENV4")
+
+ce_tff_data_8 <- ce_tff_data[grep("ENV8$", ce_tff_data$treatment),]
+ce_tff_data_8$treatment <- relevel(ce_tff_data_8$treatment, ref="ChgEnv_ED1_AS1_P125_ENV8")
+
+ce_tff_data_16 <- ce_tff_data[grep("ENV16$", ce_tff_data$treatment),]
+ce_tff_data_16$treatment <- relevel(ce_tff_data_16$treatment, ref="ChgEnv_ED1_AS1_P125_ENV16")
+
+# Non-parametric ANOVA (Kruskal-Wallis)
+kw_ce_comb_16 <- kruskal.test(fitness ~ teaser, data=ce_tff_data_16)
+dt_ce_comb_16 <- dunnTest(fitness~teaser, data=ce_tff_data_16, method="bonferroni")
+
+kw_ce_comb_8 <- kruskal.test(fitness ~ teaser, data=ce_tff_data_8)
+dt_ce_comb_8 <- dunnTest(fitness~teaser, data=ce_tff_data_8, method="bonferroni")
+
+kw_ce_comb_4 <- kruskal.test(fitness ~ teaser, data=ce_tff_data_4)
+dt_ce_comb_4 <- dunnTest(fitness~teaser, data=ce_tff_data_4, method="bonferroni")
+
+kw_ce_comb_2 <- kruskal.test(fitness ~ teaser, data=ce_tff_data_2)
+dt_ce_comb_2 <- dunnTest(fitness~teaser, data=ce_tff_data_2, method="bonferroni")
 
