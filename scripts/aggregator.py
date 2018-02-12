79b6ed6901662ecfb8de47e11915ce71b58d801d
Alex Lalejini <amlalejini@gmail.com>
2018-02-12 12:54:52 -0500

    Updating aggregator script.

--- scripts/aggregator.py
+++ scripts/aggregator.py
@@ -25,6 +25,11 @@ def main():
     parser = argparse.ArgumentParser(description="Data aggregation script.")
     parser.add_argument("data_directory", type=str, help="Target experiment directory.")
     parser.add_argument("benchmark", type=str, help="What benchmark is this?")
+
+    parser.add_argument("-u", "--update", type=int, help="Update to get fitness for. If no update is given, last line of fitness file is used.")
+    parser.add_argument("-f", "--fitness_file", type=str, help="File to pull fitness from.")
+
+    # OLD Options.
     parser.add_argument("-ff", "--final_fitness", action="store_true", help="Aggregate final fitness data from fitness.csv file.")
     parser.add_argument("-fot", "--fitness_over_time", action="store_true", help="Aggregate fitness over time from fitness.csv file.")
     parser.add_argument("-mt_ff", "--multi_trial_final_fitness", action="store_true", help="Aggregate multi-trial final fitness data.")
@@ -38,6 +43,50 @@ def main():
     # Get a list of all runs.
     runs = [d for d in os.listdir(data_directory) if "__" in d]
     runs.sort()
+
+    fit_update = args.update
+    fit_file = args.fitness_file
+
+    if (fit_file):
+        print "\nExtracting fitness information..."
+        mkdir_p(dump)
+        f_content = "benchmark,treatment,run_id,update,mean_fitness,max_fitness\n"
+        for run in runs:
+            print "  run: " + str(run)
+            run_id = run.split("__")[-1]
+            run_treat = run.split("__")[0]
+            run_dir = os.path.join(data_directory, run)
+            with open(os.path.join(run_dir, fit_file), "r") as fp:
+                fitness_contents = fp.readlines()
+            header = fitness_contents[0].split(",")
+            header_lu = {header[i].strip():i for i in range(0, len(header))}
+            fitness_contents = fitness_contents[:-1]
+            fu_content = []
+            if (fit_update != None):
+                # Look for give update.
+                target_line = None
+                for line in fitness_contents:
+                    line = line.split(",")
+                    if (line[header_lu["update"]] == fit_update):
+                        target_line = line
+                        break
+                if target_line:
+                    fu_content = target_line.split(",")
+                    fu_content = map(lambda x : x.strip(), fu_content)
+                else:
+                    print "Failed to find fitness @ update %d" % fit_update
+            else:
+                # Just use the last line.
+                fu_content = fitness_contents[-1].split(",")
+                fu_content = map(lambda x : x.strip(), fu_content)
+            # Now we're ready to extract info!
+            final_update = fu_content[header_lu["update"]]
+            mean_fitness = fu_content[header_lu["mean_fitness"]]
+            max_fitness = fu_content[header_lu["max_fitness"]]
+            f_content += ",".join([benchmark, run_treat, run_id, final_update, mean_fitness, max_fitness]) + "\n"
+        with open(os.path.join(dump, "fitness_%s.csv"), "w") as fp:
+            fp.write(f_content)
+            
 
     if (args.final_fitness):
         print "\nAggregating final fitness information..."
